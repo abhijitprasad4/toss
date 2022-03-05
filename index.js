d8d93d63e904ea291592ed80d2b112a6abc986d2
abhijitprasad4 <94544539+abhijitprasad4@users.noreply.github.com>
2022-03-05 13:09:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber = Math.random() // 0-0.9
 if (randomNumber > 0.5){
-    document.getElementById("myimage").src="image/head.png";
+    document.getElementById("myimage").src="head.png";
     document.querySelector("h3").innerHTML = "It's Head 😄";
 }
 else{
-    document.getElementById("myimage").src="image/tail.png";
+    document.getElementById("myimage").src="tail.png";
     document.querySelector("h3").innerHTML = "It's Tail 😄";
 }
